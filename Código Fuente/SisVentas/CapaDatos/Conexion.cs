83d8957ee8d7a21eb7c554871391155e4b802c88
mtorres066 <mtorres066@gmail.com>
2015-11-04 14:00:02 -0700

    ultimos ajustes
    ultimos cambios antes de entrar de lleno a revisar el codigo

--- Código Fuente/SisVentas/CapaDatos/Conexion.cs
+++ Código Fuente/SisVentas/CapaDatos/Conexion.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data.Entity;
 
 
 namespace CapaDatos
 {
     class Conexion
     {
-        public static string Cn = "Data Source=JCARLOSAD7; Initial Catalog=dbventas; Integrated Security=true";
+        //public static string Cn = "Data Source=JCARLOSAD7; Initial Catalog=dbventas; Integrated Security=true";
+        public static string Cn = "server=127.0.0.1; Database=dbventas; Uid=root; pwd=Matl066";
+        //public static string Cn = "Data Source=desarrollo-lap2\\sqlexpress; Initial Catalog=dbventas; Integrated Security=True";
 
     }
 }
